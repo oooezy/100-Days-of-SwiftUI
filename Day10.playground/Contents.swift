85740621f3d1235f16935a66b80042c08aa32fc2
eunjileee <leeeeunz@gmail.com>
2022-10-01 15:15:08 +0900

    Completed 100 Days of Swift : Day10

---
+++ Day10.playground/Contents.swift
@@ -0,0 +1,132 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+red.printSummary() // Red (2012) by Taylor Swift
+wings.printSummary() // Wings (2016) by BTS
+
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! There aren't enough days remaining.")
+        }
+    }
+}
+
+var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
+var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
+
+
+// type 1
+struct Game {
+    var score = 0
+}
+
+var game = Game()
+game.score += 10
+print("Score is now \(game.score)")
+game.score -= 3
+print("Score is now \(game.score)")
+game.score += 1
+print("Score is now \(game.score)")
+
+
+// type 2
+struct Game2 {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game2 = Game2()
+game2.score += 10
+game2.score -= 3
+game2.score += 1
+    
+//Score is now 10
+//Score is now 7
+//Score is now 8
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+struct Player {
+    let name: String
+    let number: Int
+
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
+
+
+struct BankAccount {
+    private var funds = 0 // 구조체 외부에서 액세스 불가능
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+account.funds -= 1000 // error! 
